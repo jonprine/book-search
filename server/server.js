bd1fbed3853420b63212eaf39697afc07b5dd074
Vox Humana <jonprine@gmail.com>
2020-10-25 11:55:08 -0500

    updated server with apollo and graphql

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +29,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
