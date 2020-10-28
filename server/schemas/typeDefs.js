bec2f63f5e5c9ff63d3b2ab34a43a148c7745177
Vox Humana <jonprine@gmail.com>
2020-10-28 15:37:30 -0500

    updated bookInput for remove

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -42,6 +42,7 @@ const typeDefs = gql`
     loginUser(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
 
   }
 `;
