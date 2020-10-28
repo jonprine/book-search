0595b3d6ad27abebf2abdf30b117ebcbfb075587
Vox Humana <jonprine@gmail.com>
2020-10-28 15:42:15 -0500

    added saveBook to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,6 +40,17 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
           },
+          saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id},
+                { $addToSet: { savedBooks: input } },
+                { new: true }
+              );
+              return updatedUser;
+            }
+            throw new AuthenticationError('You must be logged in!')
+          },
         }      
   };
   
