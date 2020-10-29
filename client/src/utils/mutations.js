7a65762bdc258379952011a66bfed2946bbb2302
Vox Humana <jonprine@gmail.com>
2020-10-28 21:50:57 -0500

    added login user mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,23 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          link
+          image
+        }
+      }
+    }
+  }
+`;
