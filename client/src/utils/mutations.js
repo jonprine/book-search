1d28a6d25d160b6afa8a18cbd10590917fa7aa90
Vox Humana <jonprine@gmail.com>
2020-10-28 22:13:00 -0500

    added remove book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -64,3 +64,22 @@ export const SAVE_BOOK = gql`
     }
 `;
 
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+            }
+        }
+    }
+`;
