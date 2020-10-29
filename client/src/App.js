573abd79fbfccb499cf2c9e873062538bb4ae7be
Vox Humana <jonprine@gmail.com>
2020-10-29 08:56:06 -0500

    establish appollo client in server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// add these two library import statements
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: "http://localhost:3001/graphql",
+});
 
 function App() {
   return (
